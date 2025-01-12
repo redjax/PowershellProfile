9d66ba6d88c960b3ba28c1949ada2289dfdf5dbf
redjax <none@none.com>
2025-01-12 02:07:12 -0500

    Add path helpers, edit-profile and new-symlink

--- ProfileModule/Functions/Public/Path-Helpers.ps1
+++ ProfileModule/Functions/Public/Path-Helpers.ps1
@@ -33,3 +33,77 @@ function Edit-Profile {
         & $Editor $ProfilePath
     }
 }
+
+function New-SymLink {
+    <#
+        .SYNOPSIS
+        Create a new junction/symbolic link.
+
+        .PARAMETER SrcPath
+        Path to symlink source (the path that will be symlinked to a new location).
+
+        .PARAMETER DestPath
+        Path where $SrcPath alias will live.
+
+        .PARAMETER Overwrite
+        If $DestPath is a directory/junction already, remove it or back it up before creating link.
+
+        .EXAMPLE
+        New-SymLink -SrcPath c:\path\to\src -DestPath c:\path\to\destination [-Overwrite]
+    #>
+    Param(
+        [string]$SrcPath,
+        [string]$DestPath,
+        [switch]$Overwrite
+    )
+
+    If ( Test-Path -Path $DestPath ) {
+        ## Config already exists, check if it's a directory or junction
+        $Item = Get-Item $DestPath
+
+        ## Check if path is a junction
+        If ( $Item.Attributes -band [System.IO.FileAttributes]::ReparsePoint ) {
+            Write-Host "Path is already a junction/symlink: $($DestPath)"
+            return
+        }
+
+        ## Path is a regular directory
+        Write-Warning "Path already exists and is not a junction: $($DestPath)."
+        If ( $Overwrite ) {
+            Write-Host "-Overwrite detected. Moving path '$($DestPath)' to '$($DestPath).bak' and creating junction."
+            If ( Test-Path "$($DestPath).bak" ) {
+                Write-Warning "$($DestPath).bak already exists, overwriting."
+                Remove-Item "$($DestPath).bak" -Recurse -Force
+            }
+
+            try {
+                Move-Item "$($DestPath)" "$($DestPath).bak"
+            } catch {
+                Write-Error "Error moving path '$($DestPath)' to '$($DestPath).bak'. Details: $($_.Exception.Message)"
+                return
+            }
+        } else {
+            return
+        }
+    }
+
+    Write-Host "Creating symlink from '$($SrcPath)' to '$($DestPath)'."
+    $SymlinkCommand = "New-Item -Path $($SrcPath) -ItemType SymbolicLink -Target $($DestPath)"
+
+    If ( -Not ( Test-IsAdmin ) ) {
+        Write-Warning "Script is not running as an administrator. Creating symbolic links on Windows requires administrator rights, elevating command."
+        try {
+            Start-AsAdmin -Command "$($SymlinkCommand)"
+        } catch {
+            Write-Error "Error creating symlink from '$($SrcPath)' to '$($DestPath)'. Details: $($_.Exception.Message)"
+            return
+        }
+    } else {
+        try {
+            Invoke-Expression $SymlinkCommand
+        } catch {
+            Write-Error "Error creating symlink from '$($SrcPath)' to '$($DestPath)'. Details: $($_.Exception.Message)"
+            return
+        }
+    }
+}
