9fb068d35021a09193865f0a38d027b8eb73db20
redjax <none@none.com>
2025-01-13 01:24:13 -0500

    Add Get-PublicIP module

--- ProfileModule/Functions/Public/Helpers/Helpers.ps1
+++ ProfileModule/Functions/Public/Helpers/Helpers.ps1
@@ -40,6 +40,23 @@ function Start-AsAdmin {
     }
 }
 
+function New-PSProfile {
+    if (!(Test-Path -Path $PROFILE)) {
+        Write-Warning "A Powershell profile was not found at path '$($PROFILE)'. Initializing new `$PROFILE."
+        try {
+            New-Item -ItemType File -Path $PROFILE -Force
+            Write-Output "Profile initialized at path: $($PROFILE)"
+            return
+        } catch {
+            Write-Error "Failed to initialize Powershell profile at path '$($PROFILE)'. Details: $($_.Exception.Message)"
+            return $_.Exception
+        }
+    } else {
+        Write-Output "A `$PROFILE .ps1 file already exists at path '$($PROFILE)'."
+        return
+    }
+}
+
 function Get-PowershellVersion () {
     ## Print Powershell version string
     $PowershellVersion = $PSVersionTable.PSVersion.ToString()
