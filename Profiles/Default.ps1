3b8ef1f4eb23de547f2e2cbbba9b2d6e333c7628
redjax <no@none.com>
2025-01-27 09:33:43 -0500

    Remove background init for default profile

--- Profiles/Default.ps1
+++ Profiles/Default.ps1
@@ -21,17 +21,6 @@ If ( -Not ( Test-Path -Path "$($BaseProfile)" ) ) {
     Write-Warning "Could not find base profile '$($BaseProfile)'."
 }
 else {
-    ## Load from common _Base.ps1
-    #  Wrap slow code to run asynchronously later
-    #  https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
-    # @(
-    #     {
-    #         . "$($BaseProfile)"
-    #     }
-    # ) | ForEach-Object {
-    #     Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $_
-    # } | Out-Null
-
     . "$($BaseProfile)"
 }
 
