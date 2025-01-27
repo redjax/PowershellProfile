28a5142a28b5cb8deb8980555c09e1b063c68aec
redjax <no@none.com>
2025-01-27 08:42:31 -0500

    Replace old profiles with new

--- Profiles/Default.ps1
+++ Profiles/Default.ps1
@@ -1,98 +1,37 @@
 <#
-    .SYNOPSIS
-    My custom PowerShell $PROFILE.
-
-    .DESCRIPTION
-    Loads my custom ProfileModule PowerShell module. This module has various functions and aliases
-    that I want to import when a PowerShell session loads with this profile.
-
-    Uses Register-EventEngine to run slower parts of scripts as background tasks, allowing prompt input
-    immediately and loading things like the Starship prompt in the background.
-
-    When background tasks finish, the next time the user hits Enter, CTRL-C, or anything else that produces
-    a newline the prompt will reload.
+    .SUMMARY
+    Powershell $PROFILE with Starship prompt.
 #>
 
 ## Uncomment to enable profile tracing
 # Set-PSDebug -Trace 1
 
 ## Manually set this to $false to keep profile outputs on-screen after initializing
-$ClearOnInit = $false
+$ClearOnInit = $true
 
 ## Start profile initialization timer
 $ProfileStartTime = Get-Date
 
-## Create a ManualResetEvent object for the ProfileModule import state
-$Global:ProfileModuleImported = New-Object System.Threading.ManualResetEvent $false
+$ScriptRoot = $PSScriptRoot
+$BaseProfile = "$($ScriptRoot)\_Base.ps1"
 
-## Set default parameters on various commands based on Powershell version
-if ($PSVersionTable.PSVersion -ge '3.0') {
-    $PSDefaultParameterValues = @{
-        'Format-Table:AutoSize'       = $True;
-        'Send-MailMessage:SmtpServer' = $SMTPserver;
-        'Help:ShowWindow'             = $True;
-    }
-    ## Prevents the ActiveDirectory module from auto creating the AD: PSDrive
-    $Env:ADPS_LoadDefaultDrive = 0
+If ( -Not ( Test-Path -Path "$($BaseProfile)" ) ) {
+    Write-Warning "Could not find base profile '$($BaseProfile)'."
+}
+else {
+    ## Load from common _Base.ps1
+    #  Wrap slow code to run asynchronously later
+    #  https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
+    @(
+        {
+            . "$($BaseProfile)"
+        }
+    ) | ForEach-Object {
+        Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $_
+    } | Out-Null
 }
 
-## Wrap slow code to run asynchronously later
-#  https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
-@(
-    {
-        ## Alter shell based on environment
-        if ( $host.Name -eq 'ConsoleHost' ) {
-            ## Powershell console/Windows Terminal
-
-            if ( $PSVersionTable.PSVersion -ge '3.0' ) {
-                ## Import PSReadLine interactive terminal
-                Import-Module -Name 'PSReadLine' -ErrorAction SilentlyContinue
-                ## Set keyboard key for accepting suggestions
-                Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-                ## Set Enter to its normal/expected behavior
-                Set-PSReadLineKeyHandler -Key Enter -Function AcceptLine
-                ## Disable audio bells
-                Set-PSReadLineOption -BellStyle None
-            }
-        }
-        ElseIf ( $host.Name -eq 'Windows PowerShell ISE Host' ) {
-            ## Powershell ISE
-            $host.PrivateData.IntellisenseTimeoutInSeconds = 5
-            ## Import ISE modules for more interactive sessions
-            $ISEModules = 'ISEScriptingGeek', 'PsISEProjectExplorer'
-            Import-Module -Name $ISEModules -ErrorAction SilentlyContinue
-        }
-        ElseIf ( $host.Name -eq 'Visual Studio Code Host' ) {
-            ## Load VSCode modules for Powershell for debugging & other integrations
-            Import-Module -Name 'EditorServicesCommandSuite' -ErrorAction SilentlyContinue
-            Import-EditorCommand -Module 'EditorServicesCommandSuite' -ErrorAction SilentlyContinue
-        }
-    },
-    {
-        try {
-            Import-Module ProfileModule
-            ## Indicate to the script that the ProfileModule was imported successfully
-            $Global:ProfileModuleImported = $true
-            ## Signal that the module was successfully imported
-            $Global:ProfileModuleImported.Set()
-        }
-        catch {
-            Write-Error "Error loading ProfileModule. Details: $($_.Exception.Message)"
-            ## Signal even if there's an error
-            $Global:ProfileModuleImported.Set()
-        }
-    },
-    {
-        ## Initialize Starship shell
-        if (Get-Command starship -ErrorAction SilentlyContinue) {
-            Invoke-Expression (& starship init powershell)
-        }
-    }
-) | ForEach-Object {
-    Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $_
-} | Out-Null
-
-if ($ClearOnInit) {
+If ( $ClearOnInit ) {
     Clear-Host
 }
 
