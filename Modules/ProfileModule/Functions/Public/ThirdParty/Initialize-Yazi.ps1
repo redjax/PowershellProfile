fc8a04e956eef032027bb1b14017a073de3663aa
redjax <none@none.com>
2025-02-01 00:48:32 -0500

    Update yazi init script, checks for yazi installed status

--- Modules/ProfileModule/Functions/Public/ThirdParty/Initialize-Yazi.ps1
+++ Modules/ProfileModule/Functions/Public/ThirdParty/Initialize-Yazi.ps1
@@ -1,11 +1,13 @@
 function y {
-    $tmp = [System.IO.Path]::GetTempFileName()
-    yazi $args --cwd-file="$tmp"
-    $cwd = Get-Content -Path $tmp -Encoding UTF8
-    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
-        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    if ( Get-Command yazi -ErrorAction SilentlyContinue ) {   
+        $tmp = [System.IO.Path]::GetTempFileName()
+        yazi $args --cwd-file="$tmp"
+        $cwd = Get-Content -Path $tmp -Encoding UTF8
+        if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+            Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+        }
+        Remove-Item -Path $tmp
     }
-    Remove-Item -Path $tmp
 }
 
 function Open-YaziHelp {
