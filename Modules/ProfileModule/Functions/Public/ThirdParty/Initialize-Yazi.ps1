22d68d74ff67341d8439bd39f4df477612507548
redjax <none@none.com>
2025-02-01 00:47:25 -0500

    Add yazi help.
    Open documentation to keybinds.

--- Modules/ProfileModule/Functions/Public/ThirdParty/Initialize-Yazi.ps1
+++ Modules/ProfileModule/Functions/Public/ThirdParty/Initialize-Yazi.ps1
@@ -7,3 +7,12 @@ function y {
     }
     Remove-Item -Path $tmp
 }
+
+function Open-YaziHelp {
+    try {
+        Start-Process "https://yazi-rs.github.io/docs/quick-start"
+    } catch {
+        Write-Error "Failed to open Yazi help page. Details: $($_.Exception.Message)"
+        exit 1
+    }
+}
