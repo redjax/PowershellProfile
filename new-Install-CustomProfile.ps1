bc1fc24a882139b37922fb9d3c792f7a7fec6e9d
redjax <no@none.com>
2025-02-12 00:41:29 -0500

    Add PowershellProfileCLI module.
    Start work on new setup script, which will use the CLI module.

---
+++ new-Install-CustomProfile.ps1
@@ -0,0 +1,37 @@
+Param(
+    [Parameter(HelpMessage = "Path to the PowershellProfileCLI module")]
+    [string]$ModulePath = (Join-Path $PSScriptRoot "scripts/setup/PowershellProfileCLI")
+)
+
+## Enable informational logging
+$InformationPreference = "Continue"
+
+if ( $PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Verbose') ) {
+    $VerbosePreference = "Continue"
+    $DebugPreference = "Continue"
+    Write-Verbose "DEBUG and VERBOSE logging enabled."
+} elseif ( $PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Debug') ) {
+    $DebugPreference = "Continue"
+    Write-Debug "DEBUG logging enabled."
+} else {
+    $VerbosePreference = "SilentlyContinue"
+    $DebugPreference = "SilentlyContinue"
+}
+
+Write-Debug "PowershellProfileCLI Module path: $($ModulePath)"
+
+Write-Information "Importing PowershellProfileCLI module from path: $($ModulePath)"
+try {
+    Import-Module $ModulePath -Force -Verbose
+} catch {
+    Write-Error "Error importing the PowershellProfileCLI module. Details: $($_.Exception.Message)"
+    exit 1
+}
+
+Write-Information "Listing PowershellProfileCLI commands."
+try {
+    Get-Command -Module Get-PowershellProfileCLI
+} catch {
+    Write-Error "Error getting the PowershellProfileCLI commands. Module may have imported incorrectly. Details: $($_.Exception.Message)"
+    exit 1
+}
