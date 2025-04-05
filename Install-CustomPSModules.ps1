71cd237b86a15d301122a59d696cf69bb682ac3e
redjax <no@none.com>
2025-04-05 01:17:29 -0400

    Update custom module install script.
    Add fancy emoji messages.
    
    Add missing import.

--- Install-CustomPSModules.ps1
+++ Install-CustomPSModules.ps1
@@ -1,21 +1,42 @@
+$ProfileSetupModulePath = "$PSScriptRoot/scripts/setup/PowershellProfileSetup"
+
+if (-not (Test-Path $ProfileSetupModulePath)) {
+    Write-Error "PowershellProfileSetup module not found at path: $ProfileSetupModulePath"
+    exit(1)
+}
+
+## Ensure there is a .psm1 file at the module path
+if ( -not ( Get-ChildItem "$PSScriptRoot/scripts/setup/PowershellProfileSetup" -Filter *.psm1 ) ) {
+    Write-Error "Path is not a module directory: $PSScriptRoot/scripts/setup/PowershellProfileSetup"
+    exit(1)
+}
+
 ## Test if Install-CustomModules command is available
 if (-not (Get-Command Install-CustomModules -ErrorAction SilentlyContinue)) {
-    Write-Error "Install-CustomModules command is not available."
-
-    Write-Output "Importing module: ./scripts/setup/PowershellProfileSetup"
+    Write-Debug "⚠️ Install-CustomModules command is not available. Import module from path: $($ProfileSetupModulePath)"
     try {
-        Import-Module ./scripts/setup/PowershellProfileSetup -ErrorAction Stop
+        Import-Module $ProfileSetupModulePath -ErrorAction Stop
     }
     catch {
-        Write-Error "Error importing PowershellProfileSetup module. Details: $($_.Exception.Message)"
+        Write-Error "❌ Error importing PowershellProfileSetup module. Details: $($_.Exception.Message)"
         exit(1)
     }
 }
 
 if (-not (Get-Command Install-CustomModules -ErrorAction SilentlyContinue)) {
-    Write-Error "Install-CustomModules command is not available."
+    Write-Error "❌ Install-CustomModules command is not available after importing module."
     exit(1)
 }
+else {
+    Write-Output "✅ Install-CustomModules command is available after importing module."
+}
 
+Write-Output "--[ Installing custom Powershell modules"
 ## Run Install-CustomModules
-# Install-CustomModules -RepoModulesDir C:\scripts\PowershellProfile\Modules
+try {
+    Install-CustomModules -RepoModulesDir C:\scripts\PowershellProfile\Modules | Out-Null
+}
+catch {
+    Write-Error "❌ Error installing custom Powershell modules. Details: $($_.Exception.Message)"
+    exit(1)
+}
