d8cac8e6ca4046164cfcdae7405136f890795f4c
redjax <no@none.com>
2025-04-06 13:21:18 -0400

    Move custom module install script to setup module.
    Add step to install modules to Install-CustomProfile.ps1

--- Modules/Setup/PowershellProfileSetup/public/Install-CustomModules.ps1
+++ Modules/Setup/PowershellProfileSetup/public/Install-CustomModules.ps1
@@ -1,80 +1,175 @@
 function Install-CustomModules {
     [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false, HelpMessage = "Path to repository's Modules directory")]
-        [string]$RepoModulesDir = $PSScriptRoot,
-        [Parameter(Mandatory = $false, HelpMessage = "List of modules to install")]
-        [string[]]$Modules = @(),
-        [Parameter(Mandatory = $false, HelpMessage = "Path where custom modules should be installed")]
-        [string]$HostCustomModulesPath
+    Param(
+        [Parameter(mandatory = $false, HelpMessage = "Path to the ProfileSetup module")]
+        [string]$SetupModulePath,
+        [Parameter(mandatory = $false, HelpMessage = "The path to the JSON config file to use for script execution.")]
+        [string]$ConfigFile = "config.json",
+        [Parameter(mandatory = $false, HelpMessage = "Path to custom modules for installation")]
+        [string]$RepoCustomModulesDir,
+        [Parameter(mandatory = $false, HelpMessage = "Path to host's custom moduless directory in the `$PROFILE path")]
+        [string]$HostCustomPSModulesDir
+
     )
 
-    ## Arrays to hold module install success/failure
-    $SuccessInstalled = @()
-    $FailInstalled = @()
+    [string]$SetupModuleFilename = "PowershellProfileSetup"
 
-    if ( -not ( Test-Path -Path $HostCustomModulesPath -ErrorAction SilentlyContinue ) ) {
-        Write-Warning "Host install path not found: $($HostCustomModulesPath)"
+    Write-Host "`$SetupModulePath=$($SetupModulePath)" -ForegroundColor Yellow
 
+    Write-Verbose "`$RepoCustomModulesDir=$($RepoCustomModulesDir)"
+    Write-Verbose "`$HostCustomPSModulesDir=$($HostCustomPSModulesDir)"
+
+    Write-Host "`n--[ Script Setup" -ForegroundColor Magenta
+
+    ## Ensure PowershellProfileSetup module is available
+    if (-not ( Test-Path $SetupModulePath ) ) {
+        Write-Error "PowershellProfileSetup module not found at path: $SetupModulePath"
+        return
+    }
+
+    ## Ensure there is a .psm1 file at the module path
+    if ( -not ( Get-ChildItem "$SetupModulePath" -Filter *.psm1 ) ) {
+        Write-Error "Path is not a module directory: $SetupModulePath"
+        return
+    }
+
+    ## Ensure repository custom Powershell modules are available
+    if ( -not ( Test-Path -Path $RepoCustomModulesDir -ErrorAction SilentlyContinue ) ) {
+        Write-Error "Repository custom Powershell modules not found at path '$RepoCustomModulesDir'."
+        return
+    }
+
+    ## Test if Install-CustomModules command is available
+    if (-not (Get-Command Install-CustomModules -ErrorAction SilentlyContinue)) {
+        Write-Debug "Install-CustomModules command is not available. Import module from path: $($SetupModulePath)"
         try {
-            New-Item -Path $HostCustomModulesPath -ItemType Directory -Force -ErrorAction Stop | Out-Null
-            Write-Debug "Created custom modules directory: $HostCustomModulesPath"
+            Import-Module $SetupModulePath -ErrorAction Stop
         }
         catch {
-            Write-Error "Error creating custom modules directory: $HostCustomModulesPath. Details: $($_.Exception.Message)"
-            return $false
+            Write-Error "Error importing PowershellProfileSetup module. Details: $($_.Exception.Message)"
+            return
         }
     }
 
-    ## Install custom modules
-    foreach ($Module in $Modules) {
-        Write-Debug "Installing custom Powershell module: $Module"
+    ## Check if Install-CustomModules is available after importing ProfileSetup module
+    if (-not (Get-Command Install-CustomModules -ErrorAction SilentlyContinue)) {
+        Write-Error "Install-CustomModules command is not available after importing module."
+        return
+    }
+    else {
+        Write-Debug "Install-CustomModules command is available after importing module."
+    }
 
-        ## Extract module name and target path
-        $ModuleName = Split-Path -Path $Module -Leaf
-        $TargetPath = Join-Path -Path $HostCustomModulesPath -ChildPath $ModuleName
+    ## Import setup module
+    Write-Host "Importing PowershellProfileSetup module from: $SetupModulePath" -ForegroundColor Cyan
+    try {
+        Import-Module $SetupModulePath -Force -Scope Global
+        Write-Host "Imported $($SetupModuleFilename) module" -ForegroundColor Green
+    }
+    catch {
+        Write-Error "Error importing PowershellProfileSetup module. Details: $($_.Exception.Message)"
+        return
+    }
 
-        if ( Test-Path -Path $TargetPath ) {
-            Write-Warning "Module already installed at path and will be overwritten: $TargetPath"
+    Write-Host "`n--[ Validate Environment" -ForegroundColor Magenta
 
-            Write-Debug "Removing path: $TargetPath"
-            try {
-                Remove-Item -Path $TargetPath -Recurse -Force -ErrorAction Stop | Out-Null
-            }
-            catch {
-                Write-Error "Error removing path: $TargetPath. Details: $($_.Exception.Message)"
+    ## Initialize custom modules directory
+    try {
+        Invoke-CustomModulesPathInit -RepoModulesDir $HostCustomPSModulesDir -ErrorAction SilentlyContinue | Out-Null
+        $CustomModulesDirCreatedStatus = $true
+    }
+    catch {
+        Write-Error "Error initializing custom Powershell modules path. Details: $($_.Exception.Message)"
+        $CustomModulesDirCreatedStatus = $false
+    }
+
+    if (-not $CustomModulesDirCreatedStatus) {
+        Write-Error "Did not find custom modules directory at path: $HostCustomPSModulesDir."
+        return
+    }
+
+    Write-Host "Initialized custom modules directory at path: $HostCustomPSModulesDir" -ForegroundColor Green
+
+    Write-Host "`n--[ Get Config" -ForegroundColor Magenta
+
+    ## Read repo configuration from config.json (or another file passed with -ConfigFile)
+    Write-Host "Reading config from '$($ConfigFile)'" -ForegroundColor Cyan
+    try {
+        $ProfileConfig = Get-ProfileConfig -ConfigFile "config.json"
+        Write-Host "Loaded configuration" -ForegroundColor Green
+    }
+    catch {
+        Write-Error "Error importing profile configuration from file: $($ConfigFile). Details: $($_.Exception.Message)"
+        return
+    }
+
+    Write-Host "`n--[ Prepare Modules for Installation" -ForegroundColor Magenta
+
+    ## Array to store module PSCustomObjects loaded from config file
+    [PSCustomObject[]]$ConfigInstallModules = @()
+    ## Array to store paths to modules for installation operation
+    [string[]]$ModuleInstallPaths = @()
+
+    ## Iterate over custom_modules list from config
+    $ProfileConfig.custom_modules | ForEach-Object {
+        ## Extract module name
+        $ModuleName = $_
+        ## Build module install path
+        $ModuleFile = Join-Path -Path $RepoCustomModulesDir -ChildPath "$($ModuleName).psm1"
+        ## Get path to module's parent dir
+        $ModuleParentDir = Join-Path -Path $RepoCustomModulesDir -ChildPath $ModuleName
+
+        ## Build module PSCustomObject
+        [PSCustomObject]$ModuleObj = [PSCustomObject]@{
+            Name = $ModuleName
+            File = $ModuleFile
+            Path = $ModuleParentDir
+        }
+
+        Write-Debug "Module: $($ModuleObj.Name), Path: $($ModuleObj.Path)"
+
+        ## Append full object to array
+        $ConfigInstallModules += $ModuleObj
+        ## Append module path to array
+        $ModuleInstallPaths += $ModuleObj.Path
+    }
+
+    Write-Host "Found $($ConfigInstallModules.Count) custom Powershell modules to install" -ForegroundColor Cyan
+
+    Write-Debug "Module install paths: $($ModuleInstallPaths -join ', ')"
+
+    Write-Host "`n--[ Install Modules" -ForegroundColor Magenta
+
+    ForEach ( $SourcePath in $ModuleInstallPaths ) {
+        $ModuleName = Split-Path -Leaf $SourcePath
+
+        try {
+            ## Validate source path exists before copying
+            if ( -not ( Test-Path -Path $SourcePath -ErrorAction SilentlyContinue ) ) {
+                Write-Warning "Module source path not found: $SourcePath. Skipping install."
                 continue
             }
-        }
 
-        try {
-            ## Copy the entire module directory
-            Copy-Item -Path $Module -Destination $TargetPath -Recurse -Force
+            ## Destination path for the module in HostCustomPSModulesDir
+            $DestinationPath = Join-Path -Path $HostCustomPSModulesDir -ChildPath (Split-Path -Leaf $SourcePath)
+            if ( Test-Path -Path $DestinationPath -ErrorAction SilentlyContinue ) {
+                Write-Warning "Module '$ModuleName' already installed and will be overwritten"
+            }
 
-            ## Track successful installation
-            Write-Debug "Successfully installed module: $ModuleName"
+            ## Copy the entire module folder to the destination directory
+            Copy-Item -Path $SourcePath -Destination $DestinationPath -Recurse -Force
 
-            $SuccessInstalled += $Module
+            Write-Host "Installed module '$ModuleName' to '$DestinationPath'" -ForegroundColor Green
         }
         catch {
-            ## Track failed installation and log error
-            Write-Error "Error installing custom Powershell module: $Module. Details: $($_.Exception.Message)"
-            $FailInstalled += $Module
+            Write-Error "Error installing module from '$SourcePath'. Details: $_.Exception.Message"
+            continue
         }
     }
-
-    Write-Debug "Installed $($SuccessInstalled.Count) custom Powershell modules successfully"
     
-    if ( $FailInstalled.Count -gt 0 ) {
-        $FailInstalled | ForEach-Object {
-            $ModuleName = Get-ModuleNameFromPath -ModulePath $_
-            Write-Error "Failed to install custom Powershell module: $ModuleName"
-        }
 
-        return $false
-    }
+    Write-Host "`n--[ Finished" -ForegroundColor Magenta
+    Write-Host "Successfully installed custom Powershell modules." -ForegroundColor Green
 
-    return $true
+    return $LASTEXITCODE
 }
-
-Export-ModuleMember -Function Install-CustomModules
