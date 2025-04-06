6f5440a7fdac087ba82567e0ed8472a14ea707fc
redjax <no@none.com>
2025-04-05 21:26:00 -0400

    Move environment helpers into scripts

--- Modules/ProfileModule/Functions/Public/Helpers/Env-Helpers/Set-EnvVar.ps1
+++ Modules/ProfileModule/Functions/Public/Helpers/Env-Helpers/Set-EnvVar.ps1
@@ -18,7 +18,7 @@ function Set-EnvVar {
     param(
         [string]$Name,
         [string]$Value,
-        [ValidateSet('Machine','User','Process')]
+        [ValidateSet('Machine', 'User', 'Process')]
         [string]$Target = 'User'
     )
 
@@ -32,39 +32,9 @@ function Set-EnvVar {
     }
 
     try {
-        [System.Environment]::SetEnvironmentVariable($Name,$Value,[System.EnvironmentVariableTarget]::$Target)
+        [System.Environment]::SetEnvironmentVariable($Name, $Value, [System.EnvironmentVariableTarget]::$Target)
     }
     catch {
         Write-Error "Unhandled exception setting environment variable. Details: $($_.Exception.Message)"
-    }
-}
-
-function Remove-EnvVar {
-    <#
-        Remove/unset an environment variable.
-
-        Usage:
-            Remove-EnvVar -Name <name>
-            Remove-EnvVar -Name <name> -Target Machine
-
-        Params:
-            Name: The name of the environment variable
-            Target: The scope of the environment variable. Machine, User, or Process
-
-        Example:
-            Remove-EnvVar -Name "EXAMPLE_VAR"
-            Write-Output $env:EXAMPLE_VAR
-    #>
-    param(
-        [string]$Name,
-        [ValidateSet('Machine','User','Process')]
-        [string]$Target = 'User'
-    )
-
-    try {
-        [System.Environment]::SetEnvironmentVariable($Name,$null,[System.EnvironmentVariableTarget]::$Target)
-    }
-    catch {
-        Write-Error "Unhandled exception removing environment variable. Details: $($_.Exception.Message)"
     }
 }
