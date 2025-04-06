37e4a7f0ad8b44d8ec5bab2a690958225f8c223e
redjax <no@none.com>
2025-04-05 21:24:41 -0400

    Move disk helpers into scripts

--- Modules/ProfileModule/Functions/Public/Helpers/Disk-Helpers/Show-DiskUsage.ps1
+++ Modules/ProfileModule/Functions/Public/Helpers/Disk-Helpers/Show-DiskUsage.ps1
@@ -1,13 +1,13 @@
-function Get-DiskUsage {
+function Show-DiskUsage {
     Get-PSDrive -PSProvider filesystem | Where-Object {
         $_.used -gt 0
-    } | Select-Object -Property Root,@{
-        Name = "SizeGB";
+    } | Select-Object -Property Root, @{
+        Name       = "SizeGB";
         expression = {
             ($_.used + $_.free) / 1GB -as [int]
         }
     },
     @{ Name = "UsedGB"; expression = { ($_.used / 1GB) -as [int] } },
     @{ Name = "FreeGB"; expression = { ($_.free / 1GB) -as [int] } },
-    @{ Name = "PctFree"; expression = { [math]::Round(($_.free / ($_.used + $_.free)) * 100,2) } }
+    @{ Name = "PctFree"; expression = { [math]::Round(($_.free / ($_.used + $_.free)) * 100, 2) } }
 }
