5560f449a050b09f2c38345b7158c62e91b04c96
redjax <no@none.com>
2025-04-06 23:47:28 -0400

    Add a _StarshipBase and Starship profile.

--- Profiles/Starship.ps1
+++ Profiles/Starship.ps1
@@ -5,6 +5,8 @@
 
 ## Uncomment to enable profile tracing
 # Set-PSDebug -Trace 1
+## Uncomment to enable debug logging
+# $DebugPreference = "Continue"
 
 ## Manually set this to $false to keep profile outputs on-screen after initializing
 $ClearOnInit = $true
@@ -13,7 +15,10 @@ $ClearOnInit = $true
 $ProfileStartTime = Get-Date
 
 $ScriptRoot = $PSScriptRoot
-$BaseProfile = "$($ScriptRoot)\_Base.ps1"
+$BaseProfile = "$($ScriptRoot)\_StarshipBase.ps1"
+
+## Create a ManualResetEvent object for starship's init state
+$Global:StarshipInitialized = New-Object System.Threading.ManualResetEvent $false
 
 if (-not (Test-Path -Path "$($BaseProfile)")) {
     Write-Warning "Could not find base profile '$($BaseProfile)'."
@@ -22,14 +27,38 @@ else {
     . "$($BaseProfile)"
 }
 
+# ## Initialize Starship in the background
+# #  Wrap slow code to run asynchronously later
+# #  https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
+# @(
+#     {
+#         ## Initialize Starship shell
+#         Start-StarshipInit
+#     }
+# ) | ForEach-Object {
+#     Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $_
+# } | Out-Null
+
+# if ($ClearOnInit) {
+#     Clear-Host
+# }
+
 ## Initialize Starship in the background
 #  Wrap slow code to run asynchronously later
 #  https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
 @(
     {
-        ## Initialize Starship shell
-        if (Get-Command starship -ErrorAction SilentlyContinue) {
-            Invoke-Expression (& starship init powershell)
+        try {
+            Start-StarshipInit
+            ## Indicate to the script that the ProfileModule was imported successfully
+            $Global:StarshipInitialized = $true
+            ## Signal that the module was successfully imported
+            $Global:StarshipInitialized.Set()
+        }
+        catch {
+            Write-Error "Error initializing Starship. Details: $($_.Exception.Message)"
+            ## Signal even if there's an error
+            $Global:StarshipInitialized.Set()
         }
     }
 ) | ForEach-Object {
@@ -40,13 +69,13 @@ if ($ClearOnInit) {
     Clear-Host
 }
 
+
 ## End profile initialization timer
 $ProfileEndTime = Get-Date
 ## Calculate profile init time
 $ProfileInitTime = $ProfileEndTime - $ProfileStartTime
 ## Print initialization time
 Write-Output "Profile loaded in $($ProfileInitTime.TotalSeconds) second(s)."
-Write-Output "Some commands may be unavailable for 1-3 seconds while background imports finish."
 
 ## Disable profile tracing
 Set-PSDebug -Trace 0
