9388b23fa613d20bf9e4ce6567f8c8277db9b671
redjax <no@none.com>
2025-10-28 13:22:55 -0400

    fix(y): Add 'y' alias for yazi

--- Profiles/Monolith/ProfileComponents/aliases.ps1
+++ Profiles/Monolith/ProfileComponents/aliases.ps1
@@ -209,3 +209,32 @@ if ( -not ( Get-Command wezterm -ErrorAction SilentlyContinue ) ) {
         Set-Alias -Name wezterm -Value $WezPath
     }
 }
+
+function y {
+    <#
+        .SYNOPSIS
+        Alias-like function for yazi
+
+        .DESCRIPTION
+        Wrap the yazi command to pass args & handle non-existent directories.
+    #>
+    if ( Get-Command yazi -ErrorAction SilentlyContinue ) {   
+        $tmp = [System.IO.Path]::GetTempFileName()
+        
+        try {
+            yazi $args --cwd-file="$tmp"
+        }
+        catch {
+            Write-Error "Failed to run yazi. Details: $($_.Exception.Message)"
+            exit 1
+        }
+
+        $cwd = Get-Content -Path $tmp -Encoding UTF8
+
+        if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+            Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+        }
+
+        Remove-Item -Path $tmp
+    }
+}
