457022f6d4bbdfe9f89bb2a5282927490dfc17e2
redjax <redjax@users.noreply.github.com>
2026-04-16 14:07:45 -0400

    fix(alias): Remove curl alias on Win10+

--- Profiles/software_inits.ps1
+++ Profiles/software_inits.ps1
@@ -29,3 +29,14 @@ if (Get-Command intelli-shell.exe -ErrorAction SilentlyContinue) {
     $intelliTime = ($intelliEnd - $intelliStart).TotalMilliseconds
     Write-Debug "IntelliShell initialized in $([Math]::Round($intelliTime))ms"
 }
+
+## Disable PowerShell's curl alias on Windows 10+
+#  If curl is installed with scoop, prefer the scoop alias
+$os = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+if (
+    (($os.CurrentMajorVersionNumber -as [int]) -eq 10 -and ($os.CurrentMinorVersionNumber -as [int]) -eq 0) -or
+    (($os.CurrentBuildNumber -as [int]) -ge 10240)
+) {
+    Remove-Item Alias:curl -ErrorAction SilentlyContinue
+}
+
